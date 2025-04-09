285c68bdc0d5950d80485d8bd84ebf4ab67ea61f
saurav saha <sauravsaha024@gmail.com>
2025-04-10 04:42:17 +0530

    Update script.js
    css remain

--- Alternate2 colors/script.js
+++ Alternate2 colors/script.js
@@ -7,6 +7,7 @@ startEl.addEventListener("click", changeColor);
 stopEl.addEventListener("click", stopchageColor);
 
 let AnimeId;
+let op = true;
 
 function changeColor() {
   // check if an interval has already been set up
@@ -16,7 +17,18 @@ function changeColor() {
 }
 
 function chageBox() {
-  myBoxEl.className = myBoxEl.className === "go" ? "stop" : "go";
+  // myBoxEl.className = myBoxEl.className === "go" ? "stop" : "go";
+    if (op) {
+    myBoxEl.className = "go";
+    boxx.className = "goA";
+    op = !op;
+  } else if (!op) {
+    myBoxEl.className = "stop";
+    boxx.className = "goB";
+
+    op = true;
+  }
+  
 }
 
 function stopchageColor() {
